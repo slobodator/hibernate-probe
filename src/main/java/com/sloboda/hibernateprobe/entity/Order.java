9bb82712300c0b53793b869e5aed87c7bdce54e0
Andriy Slobodyanyk <aslobodyanyk@luxoft.com>
2020-04-19 16:30:17 +0300

    ManyToOne lazy

--- src/main/java/com/sloboda/hibernateprobe/entity/Order.java
+++ src/main/java/com/sloboda/hibernateprobe/entity/Order.java
@@ -13,6 +13,7 @@ import javax.persistence.Convert;
 import javax.persistence.ElementCollection;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -35,7 +36,7 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(cascade = CascadeType.PERSIST)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     private Client client;
 
     @Convert(converter = OrderStatusConverter.class)
