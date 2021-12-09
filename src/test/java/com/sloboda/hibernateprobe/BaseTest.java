ff8aa918ad0a469eed649cdabf730463978e8811
Andriy Slobodyanyk <andriy.slobodyanyk@rakuten.com>
2021-12-09 17:36:50 +0100

    clean up

--- src/test/java/com/sloboda/hibernateprobe/BaseTest.java
+++ src/test/java/com/sloboda/hibernateprobe/BaseTest.java
@@ -1,15 +1,12 @@
 package com.sloboda.hibernateprobe;
 
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
 @Rollback(false)
